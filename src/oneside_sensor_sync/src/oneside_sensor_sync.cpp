9ec0561cf639e09dfd58060ec485e9d880f8876a
Xyneef <Fabio_Bottalico@student.uml.edu>
2022-02-01 15:15:06 -0500

    compiled with message filter, sensor_msgs libraries

--- src/oneside_sensor_sync/src/oneside_sensor_sync.cpp
+++ src/oneside_sensor_sync/src/oneside_sensor_sync.cpp
@@ -1,11 +1,16 @@
 #include "ros/ros.h"
-
+#include "sensor_msgs/Imu.h"
+#include "sensor_msgs/MagneticField.h"
+#include "std_msgs/Float32.h"
+#include "message_filters/subscriber.h"
+#include "message_filters/time_synchronizer.h"
+#include "message_filters/sync_policies/approximate_time.h"
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "oneside_sensor_sync");
     ros::NodeHandle n;
 
-
+    message_filters::Subscriber<sensor_msgs::Imu> imu_sub(n, "data", 1);
 
     return 0;
 }
