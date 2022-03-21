ebf4fc5f0baa12710fc9cd5fdbae3c2d9b8b1c09
Austin Mackey <austin.thomas.mackey@gmail.com>
2022-03-21 10:13:19 -0400

    current state of oneside sensor sync. DO not like that way averaging happens.

--- src/oneside_sensor_sync/src/oneside_sensor_sync.cpp
+++ src/oneside_sensor_sync/src/oneside_sensor_sync.cpp
@@ -22,41 +22,25 @@ struct buff_struct {
     float magnetic_field[3][BUFFER_LENGTH] = {0};
 };
 
-void callback(ros::Publisher& pub, const sensor_msgs::ImuConstPtr &imu, const sensor_msgs::MagneticFieldConstPtr &mag, const distance_sensor::Float32StampedConstPtr &laser, const sensor_msgs::ImageConstPtr &img) {
+void callback(ros::Publisher& pub, const sensor_msgs::ImuConstPtr &imu_ptr, const sensor_msgs::MagneticFieldConstPtr &mag_ptr, const distance_sensor::Float32StampedConstPtr &laser_ptr, const sensor_msgs::ImageConstPtr &img_ptr) {
     
     oneside_sensor_sync::Oneside msg;
+    
     msg.header.stamp = ros::Time::now();
     msg.header.frame_id = "oneside message with Imu, MagneticField, Image, and Laser Distance.";
-    msg.laser = laser->data;
-    
-    msg.imu.header = imu->header;
-    msg.imu.orientation = imu->orientation;
-    msg.imu.orientation_covariance = imu->orientation_covariance;
-    msg.imu.angular_velocity = imu->angular_velocity;
-    msg.imu.angular_velocity_covariance = imu->angular_velocity_covariance;
-    msg.imu.linear_acceleration = imu->linear_acceleration;
-    msg.imu.linear_acceleration_covariance = imu->linear_acceleration_covariance;
+    //---
+    msg.laser = laser_ptr->data;
+    msg.imu = *imu_ptr;
+    msg.mag = *mag_ptr;
+    msg.image = *img_ptr;
 
-    msg.mag.header = mag->header;
-    msg.mag.magnetic_field = mag->magnetic_field;
-    msg.mag.magnetic_field_covariance = mag->magnetic_field_covariance;
-
-    msg.image.header = img->header;
-    msg.image.height = img->height;
-    msg.image.width = img->width;
-    msg.image.encoding = img->encoding;
-    msg.image.is_bigendian = img->is_bigendian;
-    msg.image.step = img->step;
-    msg.image.data = img->data;
-
-
-    // cout << "IMU time stamp: " << imu->header.stamp << " || " << "Magnetometer time stamp: " << mag->header.stamp << endl;
-    // cout << "Laser time stamp: " << laser->header.stamp << " || " << "Image time stamp: " << img->header.stamp << endl;
+    cout << "IMU time stamp: " << imu_ptr->header.stamp << " || " << "Magnetometer time stamp: " << mag_ptr->header.stamp << endl;
+    cout << "Laser time stamp: " << laser_ptr->header.stamp << " || " << "Image time stamp: " << img_ptr->header.stamp << endl;
 
     pub.publish(msg);
 }
 
-void callback_avg(ros::Publisher& imu_pub, ros::Publisher& mag_pub, struct buff_struct& buff, const sensor_msgs::ImuConstPtr &imu, const sensor_msgs::MagneticFieldConstPtr &mag) {
+void callback_avg(ros::Publisher& imu_pub, ros::Publisher& mag_pub, struct buff_struct& buff, const sensor_msgs::ImuConstPtr& imu, const sensor_msgs::MagneticFieldConstPtr& mag) {
     int indx = buff.buffer_indx;
     float imu_orientation_avg[3] = {0.0};
     float imu_angular_velocity_avg[3] = {0.0};
@@ -81,10 +65,6 @@ void callback_avg(ros::Publisher& imu_pub, ros::Publisher& mag_pub, struct buff_
         }
     }
 
-    for (int j=0; j<3; j++) {
-        cout << "Avg " << imu_orientation_avg[j] << " || " << "Raw " << imu->orientation.x << endl;
-    }
-    
     // populate a new message with the averaged values an publish
     imu_msg.header = imu->header;
     imu_msg.orientation.x = imu_orientation_avg[0]; imu_msg.orientation.y = imu_orientation_avg[1]; imu_msg.orientation.z = imu_orientation_avg[2]; 
@@ -107,20 +87,45 @@ void callback_avg(ros::Publisher& imu_pub, ros::Publisher& mag_pub, struct buff_
 
 int main(int argc, char **argv) {
     struct buff_struct buff;
+    bool mean_option = false;
+    int c;
 
     ros::init(argc, argv, "oneside_sensor_sync");
     ros::NodeHandle n;
 
+    while ((c = getopt(argc, argv, "m")) != -1) {
+        switch (c) {
+            // choose if the one sided sync is averaged or not 
+            case 'm':
+                mean_option = true;
+                break;
+            case '?':
+                if (isprint(optopt))
+                    fprintf(stderr, "Unknown option '-%c'\n", optopt);
+                    fprintf(stderr, "Usage: oneside_sensor_sync <-m:for averaging>\n");
+                    return 1;
+        }
+    }
+
+
+
+    // publishers
     ros::Publisher oneside_pub = n.advertise<oneside_sensor_sync::Oneside>("oneside_syncd", 100);
+    ros::Publisher oneside_avg_pub = n.advertise<oneside_sensor_sync::Oneside>("oneside_avg_syncd", 100);
     ros::Publisher imu_avg_pub = n.advertise<sensor_msgs::Imu>("oneside_syncd/imu_avg", 100);
     ros::Publisher mag_avg_pub = n.advertise<sensor_msgs::MagneticField>("oneside_syncd/mag_avg", 100);
 
+    // subscribers when not averaging
     message_filters::Subscriber<sensor_msgs::Imu> imu_sub(n, "/IMU/imu/data", 1);
     message_filters::Subscriber<sensor_msgs::MagneticField> mag_sub(n, "/IMU/imu/mag", 1);
     message_filters::Subscriber<distance_sensor::Float32Stamped> laser_sub(n, "/Laser/distance", 1);
     message_filters::Subscriber<sensor_msgs::Image> img_sub(n, "/camera/left_camera/image_raw", 1);
 
-    // a callback for averaging imu and mag data
+    // subscribers when averaging
+    message_filters::Subscriber<sensor_msgs::Imu> imu_avg_sub(n, "oneside_syncd/imu_avg", 1);
+    message_filters::Subscriber<sensor_msgs::MagneticField> mag_avg_sub(n, "oneside_syncd/mag_avg", 1);
+
+    // a callback for averaging imu and mag data and publishing on oneside_syncd/*_avg
     typedef sync_policies::ApproximateTime<sensor_msgs::Imu, sensor_msgs::MagneticField> MyAsyncPolicy_avg;
     Synchronizer<MyAsyncPolicy_avg> sync_avg(MyAsyncPolicy_avg(10), imu_sub, mag_sub);
     
@@ -128,10 +133,17 @@ int main(int argc, char **argv) {
     
     // Approximate time syncronozation takes in mag, imu, laser distance, and image_raw sensor messages and combines them
     // the slowest transmitting sensor dictates the transmission rate as async policy will wait for a complete set of all 4 sensors before transmitting one message.
-    typedef sync_policies::ApproximateTime<sensor_msgs::Imu, sensor_msgs::MagneticField, distance_sensor::Float32Stamped, sensor_msgs::Image> MyAsyncPolicy;
-    Synchronizer<MyAsyncPolicy> sync(MyAsyncPolicy(10), imu_sub, mag_sub, laser_sub, img_sub);
-    
-    sync.registerCallback(boost::bind(&callback, boost::ref(oneside_pub), _1, _2, _3, _4));
+    //if (mean_option) {
+        typedef sync_policies::ApproximateTime<sensor_msgs::Imu, sensor_msgs::MagneticField, distance_sensor::Float32Stamped, sensor_msgs::Image> MyAsyncPolicy_sync_avg;
+        Synchronizer<MyAsyncPolicy_sync_avg> main_sync_avg(MyAsyncPolicy_sync_avg(10), imu_avg_sub, mag_avg_sub, laser_sub, img_sub);
+        main_sync_avg.registerCallback(boost::bind(&callback, boost::ref(oneside_avg_pub), _1, _2, _3, _4));
+    //}
+    //else {
+        typedef sync_policies::ApproximateTime<sensor_msgs::Imu, sensor_msgs::MagneticField, distance_sensor::Float32Stamped, sensor_msgs::Image> MyAsyncPolicy_sync;
+        Synchronizer<MyAsyncPolicy_sync> main_sync(MyAsyncPolicy_sync(10), imu_sub, mag_sub, laser_sub, img_sub);
+        main_sync.registerCallback(boost::bind(&callback, boost::ref(oneside_pub), _1, _2, _3, _4));
+    //}
+
 
     ros::spin();
 
